e3f21041a72fed12c5208671e4f329a788d74f96
salman49 <salman@steyp.com>
2023-05-09 13:08:26 +0530

    phone auth with verify otp

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import {View, Text, SafeAreaView} from 'react-native';
+
+// components
 import Login from './src/components/screens/Login';
 import Signup from './src/components/screens/Signup';
+import PhoneNumber from './src/components/screens/PhoneNumber';
 
 function App() {
   return (
     <SafeAreaView>
       {/* <Signup /> */}
-      <Login />
+      {/* <Login /> */}
+      <PhoneNumber />
     </SafeAreaView>
   );
 }
--- src/components/screens/PhoneNumber.js
+++ src/components/screens/PhoneNumber.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   StyleSheet,
   Text,
@@ -7,13 +7,32 @@ import {
   View,
 } from 'react-native';
 
-export default function PhoneNumberInput() {
-  const [phoneNumber, setPhoneNumber] = useState('');
+import auth from '@react-native-firebase/auth';
 
-  function handleConfirm() {
-    // Handle the confirmation button press
-    console.log(`Phone number entered: ${phoneNumber}`);
+export default function PhoneNumber() {
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [verifyOtp, setVerifyOtp] = useState('');
+
+  // should send phonenumber with countrycode
+  async function signInWithPhoneNumber(phoneNumber) {
+    const confirmation = await auth().signInWithPhoneNumber(phoneNumber);
+    console.log(confirmation, '++');
   }
+
+  // confirm otp function
+  const confirmCode = async code => {
+    try {
+      const credential = auth.PhoneAuthProvider.credential(
+        'AL3R4eRgt5poN6EG39IYbFQHEA_cCljGsmJx4D45HOUlCe4EPPBPOx5eVkzLmn-pu_-gd-4fr9tw8sIiDUIzg17L0bptZ7CqNqhsqMxb0XrhWgP8sTBpsqJ-MdlvHUoqJFOG2Uolt8qSnB4mJIDecdjuQP0kDITbuCuWRdmYx-GHeUjPHBq3d5tiKYKUMKL-ebbFnUbrh4aiDKQ7d-4qZgDrKUiG3Q5kpQb21WYMUuUgbSqvw2EwVrg',
+        code,
+      );
+      await auth().signInWithCredential(credential);
+      console.log('user oke');
+    } catch (error) {
+      console.log(error, '++');
+    }
+    console.log(code);
+  };
 
   return (
     <View style={styles.container}>
@@ -25,7 +44,26 @@ export default function PhoneNumberInput() {
           onChangeText={text => setPhoneNumber(text)}
         />
       </View>
-      <TouchableOpacity style={styles.button} onPress={handleConfirm}>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          signInWithPhoneNumber(phoneNumber);
+        }}>
+        <Text style={styles.buttonText}>Confirm</Text>
+      </TouchableOpacity>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Verify otp"
+          keyboardType="phone-pad"
+          onChangeText={text => setVerifyOtp(text)}
+        />
+      </View>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          confirmCode(verifyOtp);
+        }}>
         <Text style={styles.buttonText}>Confirm</Text>
       </TouchableOpacity>
     </View>
@@ -33,11 +71,7 @@ export default function PhoneNumberInput() {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container: {},
   inputContainer: {
     flexDirection: 'row',
     alignItems: 'center',
